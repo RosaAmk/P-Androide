127199ed4986e007f4883f470d77f17cc794252c
RosaAmk <36438699+RosaAmk@users.noreply.github.com>
2021-04-07 13:20:27 +0200

    update

--- src/agent.py
+++ src/agent.py
@@ -8,17 +8,28 @@ import random
 from math import exp
 import numpy as np
 class Agent():
-        def __init__(self):
+        def __init__(self,w):
             super().__init__()
-            self.g_skill = random.uniform(-1,1)
-            self.neighbours = []
+            self.g_skill = round(random.uniform(-1,1),1)
             self.fitness = 0
             self.genomeList = []
             self.energy = 4
             self.wait = 0
+            self.max_energy = 6
+            self.sigma = 0.1
+            self.listening = 0
+            self.fitness_vector = []
+            self.w=w
             
         def __str__(self):
             return " ".join((str(self.g_skill),str(self.fitness)))
+        
+        def reactivate(self):
+            self.energy = 4
+            self.genomeList = []
+            self.fitness = 0
+            self.wait = 0
+            self.listening = 0
         
         def add_neighbour(self,agent):
             self.neighbours.append(agent)
@@ -27,43 +38,65 @@ class Agent():
             for a in agents:
                 self.add_neighbour(a)
             
-        def broadcast(self):
-            if not self.is_stopped():
-                for a in self.neighbours:
-                    if not a.is_stopped():
-                        a.genomeList.append(((self.g_skill , self.fitness)))
+        def broadcast(self,a):
+            if not self.is_stopped() and len(self.fitness_vector)>= self.w :
+                if a.wait == 0:
+                        a.genomeList.append((self.g_skill,self.fitness))
 
         def apply_variation_random(self):
-            if not self.is_stopped():
-                if self.genomeList:
-                    self.g_skill = random.choice(self.genomeList)[0]
-                self.genomeList = []
-        def apply_variation_fitness_prop(self):
-            if not self.is_stopped():
-                if self.genomeList:
-                    x = [elem[1] for elem in self.genomeList]
-                    if sum(x)== 0:
-                        self.g_skill = np.random.choice([elem[0] for elem in self.genomeList] )
-                    else:
-                        self.g_skill = np.random.choice([elem[0] for elem in self.genomeList],p= np.array(x)/sum(x) )
-                self.genomeList = []
-                
-        def apply_variation_rank_prop(self):
-            if not self.is_stopped():
-                if self.genomeList:
-
-                    x = list(range(len(self.genomeList)))
-                    if sum(x)== 0:
-                        self.g_skill = np.random.choice([elem[0] for elem in self.genomeList] )
-                    else:
-                        self.g_skill = np.random.choice([elem[0] for elem in sorted(self.genomeList, key=lambda tup: tup[1])],p= np.array(x)/sum(x) )
-               
+            
+                liste = self.genomeList
+                if liste:           
+                    self.g_skill = round(np.random.choice([elem[0] for elem in liste] ) + random.gauss(0, self.sigma),1)
+                    self.g_skill = min(1, self.g_skill)
+                    self.g_skill = max(-1, self.g_skill)
                     self.genomeList = []
+                else:
+                    self.g_skill = None
+
+        def apply_variation_fitness_prop(self):
+                liste = self.genomeList
+                if liste:
+                    x = [elem[1] for elem in liste]
+                    if sum(x)== 0:
+                        self.g_skill = round(np.random.choice([elem[0] for elem in liste] ) + random.gauss(0, self.sigma),1)
+                        self.g_skill = min(1, self.g_skill)
+                        self.g_skill = max(-1, self.g_skill)
+                    else:
+                        self.g_skill = round(np.random.choice([elem[0] for elem in liste],p= np.array(x)/sum(x) ) + random.gauss(0, self.sigma),1)
+                        self.g_skill = min(1, self.g_skill)
+                        self.g_skill = max(-1, self.g_skill)
+                    self.genomeList = []
+                else:
+                    self.g_skill = None
+
+        def apply_variation_rank_prop(self):
+                liste = self.genomeList
+                if liste:
+
+                    x = list(range(1, len(liste)+1))
+                    if sum(x)== 0:
+                        self.g_skill = round(np.random.choice([elem[0] for elem in liste] ) + random.gauss(0, self.sigma),1)
+                        self.g_skill = min(1, self.g_skill)
+                        self.g_skill = max(-1, self.g_skill)
+                    else:
+                        self.g_skill = round(np.random.choice([elem[0] for elem in sorted(liste, key=lambda tup: tup[1])],p= np.array(x)/sum(x) )  + random.gauss(0, self.sigma),1)
+                        self.g_skill = min(1, self.g_skill)
+                        self.g_skill = max(-1, self.g_skill)
+                    self.genomeList = []
+                else:
+                    self.g_skill = None
         def apply_variation_fitness(self):
-            if not self.is_stopped():
-                if self.genomeList:
-                    self.g_skill = max(self.genomeList, key = lambda i : i[1])[0]
-                self.genomeList = []
+                liste = self.genomeList
+                if liste:
+                    c = random.gauss(0, self.sigma)
+                    #print(c , max(self.genomeList, key = lambda i : i[1])[0]  )
+                    self.g_skill = round(max(liste, key = lambda i : i[1])[0]  + c,1)
+                    self.g_skill = min(1, self.g_skill)
+                    self.g_skill = max(-1, self.g_skill)
+                    self.genomeList = []
+                else:
+                    self.g_skill = None
         def get_neighbours(self):
             return self.neighbours
                 
@@ -79,19 +112,23 @@ class Agent():
                 return 0
             if self.g_skill > 0 :    
                 env.R1 -= 1
-                return 1/(1 + exp(50*(-1*self.g_skill+0.5)))
+                return 1/(1 + exp(10*(-2*self.g_skill+1)))
             if self.g_skill < 0 :
                 env.R2 -= 1
-                return 1/(1 + exp(50*((self.g_skill)+0.5)))
-            return 0
+                return 1/(1 + exp(10*((2*self.g_skill)+1)))
+            else:
+                return 0
 
-        def compute_fitness(self, env,it):   
-            
+        def compute_fitness(self, env):
             if not self.is_stopped():
-                x = self.f_syn(env)
-                self.energy += x
-                if it > 0:
-                    self.fitness += x
+                x=self.f_syn(env)
+                self.fitness_vector.append(x)
+                self.energy = min(self.max_energy , self.energy+ x)                
+                if len(self.fitness_vector) > self.w :
+                    self.fitness_vector.pop(0)
+                    self.fitness =sum(self.fitness_vector)
+
+
             
         ''' def get_group(self):
 
@@ -101,29 +138,33 @@ class Agent():
                 return 1'''
 
             
-        def move(self):
+        def move(self, env):
             if self.is_stopped():
-                self.charge()
+                env.R1 -= 0.5
+                env.R2 -= 0.5
+                if self.g_skill!= None:
+                    self.charge()
             else :
-                self.energy -= 1
-                if self.energy <= 0:
-                    self.genomeList = []
+                self.energy = max(0 , self.energy-1)
+                if self.energy == 0:
+                    #self.genomeList = []
                     self.wait = random.randint(4, 15)
 
         
         def is_stopped(self):
-            if self.wait > 0:
+            if self.wait > 0  or self.listening >0 or self.g_skill == None:
                 return True
             else:
                 return False
         
         
         def charge(self):
-            self.wait -= 1
-            if self.wait == 0:
-                self.energy = 4
+            if self.wait > 0:
+                self.wait -= 1
+                if self.wait == 0:
+                    self.energy = 4
+                    self.listening = 4
+            else:
+                self.listening -= 1
+        
                 
-                
-
-if __name__ == '__main__':
-    print("rger")
--- src/main.py
+++ src/main.py
@@ -4,31 +4,39 @@ Created on Fri Feb 12 20:51:21 2021
 
 @author: roza
 """
-from agent import Agent
-from graph_alea import Graph
-
+from agent2 import Agent
+from gen_graph import Graph
+import random
 from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
-
-nb_agent = 200
+import graphviz as pgv
+import plot
+nb_agent = 500
 nb_it = 1000
+lifetime = 400
+w = 100
+density = 0.004
 class Env():
     def __init__(self):
-        self.R1 = 0
-        self.R2 = 0
-        
+        self.R1 = nb_agent*nb_it *lifetime/2
+        self.R2 = nb_agent*nb_it *lifetime/2        
 
 def gen_from_density(n,d): 
     g1 = Graph(n,d)
     g = []
     for i in range(n):
-        g.append(Agent())
+        g.append(Agent(w))
+    
     g1.gen_graph()
+    '''
+    graph = pgv.Digraph()
+    for i in range(n):
+        graph.node(str(g[i].g_skill))
     for e in g1.edges_final:
-        g[e[0]].add_neighbour(g[e[1]])
-        g[e[1]].add_neighbour(g[e[0]])
-    return g
+        graph.edge(str(g[e[0]].g_skill), str(g[e[1]].g_skill)) 
+    '''
+    return g, g1.edges_final
 
 def gen_star(n):
     g = []
@@ -76,33 +84,83 @@ def gen_chaine(n):
     return g
         
 if __name__ == '__main__':
-    for j in range(10):
-        c = Counter()
-        env = Env()
-        graph = gen_from_density(nb_agent, 0.01)
-        i = 0
-        for i in range(nb_it):
-            env.R1 = nb_agent/2
-            env.R2 = nb_agent/2
-            i += 1
-            #print('##############')
-            k = 0
-            for a in graph:
-                a.move()
-                a.compute_fitness(env, i)
-                a.broadcast()
-                #print(k , a.is_stopped(),a.g_skill, a.energy, a.fitness)
-                k += 1
-            for a in graph:
-                a.apply_variation_fitness_prop()
-        for a in graph:
-            c[round(a.g_skill,1)] += 1
+    res = dict()
+    methods = [ 'random','fitness prop', 'rank prop', 'Best']
+    gskills = dict()
 
-        labels, values = zip(*sorted(c.items()))
+    for method in range(4):
+        print(methods[method])
+        c2 = Counter()
+        c3 = []
+        for j in range(10):
+            i = 0
+            for e in np.linspace(-1,1,21):
+                gskills[round(e,1)] = i
+                i += 1
+                gskills_mat =np.zeros((21,nb_it//100))
+            graph,edges = gen_from_density(nb_agent,density)
+            
+            env = Env()
+
+            for i in range(nb_it):
+                print(i)
+                for k in range(lifetime):
+                    env.R1 = nb_agent/2
+                    env.R2 = nb_agent/2    
+                    for a in graph:
+                        
+                        a.move(env)
+                        a.compute_fitness(env)
+                    for e in edges:
+                        graph[e[0]].broadcast(graph[e[1]])
+                        graph[e[1]].broadcast(graph[e[0]])
+                        #print(0 , a.is_stopped(),a.g_skill, a.energy, a.fitness)
+                for a in graph:
+                    if method == 0:
+                        a.apply_variation_random()
+                    if method == 1:                        
+                        a.apply_variation_fitness_prop()                      
+                    if method == 2:
+                        a.apply_variation_rank_prop()
+                    if method == 3:
+                        a.apply_variation_fitness()
+
+                    if i%100 == 0:
+                        if a.g_skill != None:
+                            gskills_mat[gskills[round(a.g_skill,1)] , i//100 ] += 1
+
+                            
+                            
+
+                
+                
+            cpt = 0   
+            for a in graph:
+                if not a.is_stopped() :
+                        cpt  += 1
+                        
+            #c2[cpt] += 1
+            c3.append(cpt)
+            #labels, values = zip(*sorted(c.items()))
+            #indexes = np.arange(len(labels))
+            plot.heatmap_plot([(i+1)*100 for i in range(nb_it//100)] ,gskills.keys(),  gskills_mat , methods[method])
+            #plt.bar(indexes, values)
+            #plt.xticks(indexes , labels)
+            #plt.show()
+            
+        ''' 
+        labels, values = zip(*sorted(c2.items()))
         indexes = np.arange(len(labels))
-
+    
         plt.bar(indexes, values)
         plt.xticks(indexes , labels)
-        plt.show()
-   
-            
+        plt.show()'''
+        res[method] = c3
+    
+    data=[]
+    labels=[]
+    
+    for k in res.keys():
+        data.append(res[k])
+        labels.append(methods[k])
+    plot.violin_plots(data, labels, 'Violon plots with density == 0,004 with 500 agents')
